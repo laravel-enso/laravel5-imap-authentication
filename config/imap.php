0b9b196c6f1e61ed841ef36355061f9119ca654a
aocneanu <aocneanu@gmail.com>
2017-03-21 14:13:09 +0000

    Apply fixes from StyleCI

--- config/imap.php
+++ config/imap.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "server" => env('IMAP_AUTH_SERVER', 'localhost'),
-    "port" => env('IMAP_PORT', 993),
-    "parameters" => env('IMAP_PARAMETERS', '')
+    'server'     => env('IMAP_AUTH_SERVER', 'localhost'),
+    'port'       => env('IMAP_PORT', 993),
+    'parameters' => env('IMAP_PARAMETERS', ''),
 ];
